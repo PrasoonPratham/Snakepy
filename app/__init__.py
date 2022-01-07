77116a2ed43b5b2b2969dac04ef56828b80ff266
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2022-01-07 22:49:12 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,23 +3,23 @@ import pygame
 from .config import Colors
 
 # Grid draw func.
-def draw_grid(surface, size, height, width):
-    for x in range(0, height):
-        for y in range(0, width):
+def draw_grid(surface, grid_size, grid_height, grid_width):
+    for y in range(0, int(grid_height)):
+        for x in range(0, int(grid_width)):
             if (x + y) % 2 == 0:
-                # Rect
-                ret = pygame.Rect((x * size, y * size), (size, size))
-                pygame.draw.rect(surface, Colors.DARK_BLUE, ret)
+                r = pygame.Rect((x * grid_size, y * grid_size), (grid_size, grid_size))
+                pygame.draw.rect(surface, Colors.DARK_BLUE, r)
             else:
-                ret = pygame.Rect((x * size, y * size), (size, size))
-                pygame.draw.rect(surface, Colors.LIGHT_BLUE, ret)
+                rr = pygame.Rect((x * grid_size, y * grid_size), (grid_size, grid_size))
+                pygame.draw.rect(surface, Colors.LESS_DARK_BLUE, rr)
 
 
 class Game:
     # Constants.
     GRID_SIZE = 20
-    GRID_WIDTH = 800 / GRID_SIZE
-    GRID_HEIGHT = 800 / GRID_SIZE
+    GRID_WIDTH = 800 // GRID_SIZE
+    GRID_HEIGHT = 800 // GRID_SIZE
+    SIDE = 800
 
     # Initialize the game.
     def __init__(self):
@@ -27,13 +27,16 @@ class Game:
         pygame.init()
 
         # Window.
-        self.window = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), 0, 32)
+        self.window = pygame.display.set_mode((self.SIDE, self.SIDE), 0, 32)
+
+        # Clock.
+        self.clock = pygame.time.Clock()
 
         # Set the caption for the window.
         pygame.display.set_caption("Snake Game")
 
         # Surface.
-        self.surface = pygame.Surface(self.screen.get_size())
+        self.surface = pygame.Surface(self.window.get_size())
         self.surface = self.surface.convert()
 
         # Draw grid.
